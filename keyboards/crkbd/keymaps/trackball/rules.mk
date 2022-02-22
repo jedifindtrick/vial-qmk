cf35464dce1087dbad689c36f6882625ece35716
Alexander <48211557+jedifindtrick@users.noreply.github.com>
2022-02-22 22:38:14 +0100

    Added trackballsupport

---
+++ keyboards/crkbd/keymaps/trackball/rules.mk
@@ -0,0 +1,6 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+POINTING_DEVICE_ENABLE = yes
+SRC += \
+	split_scomm.c \
+	paw3204.c \
--- keyboards/crkbd/keymaps/vial/rules.mk
+++ keyboards/crkbd/keymaps/vial/rules.mk
@@ -1,11 +1,11 @@
 MOUSEKEY_ENABLE     = no      # Mouse keys
-RGBLIGHT_ENABLE     = yes     # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE     = no     # Enable WS2812 RGB underlight.
 VIA_ENABLE          = yes     # Enable VIA
 VIAL_ENABLE         = yes     # Enable VIAL
-OLED_ENABLE         = no
+OLED_ENABLE         = yes
 OLED_DRIVER         = SSD1306
 EXTRAKEY_ENABLE     = yes
 LTO_ENABLE          = yes
-QMK_SETTINGS        = no
+QMK_SETTINGS        = yes
 COMBO_ENABLE        = no
 
