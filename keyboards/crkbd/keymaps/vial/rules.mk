e5d4733e5b5c273ad39222ddbbddfc15157ef0f2
Alexander <48211557+jedifindtrick@users.noreply.github.com>
2022-06-28 11:40:17 +0200

    Update rules.mk

--- keyboards/crkbd/keymaps/vial/rules.mk
+++ keyboards/crkbd/keymaps/vial/rules.mk
@@ -1,11 +1,10 @@
-MOUSEKEY_ENABLE     = no      # Mouse keys
+MOUSEKEY_ENABLE     = yes      # Mouse keys
 RGBLIGHT_ENABLE     = no     # Enable WS2812 RGB underlight.
 VIA_ENABLE          = yes     # Enable VIA
 VIAL_ENABLE         = yes     # Enable VIAL
-OLED_ENABLE         = yes
+OLED_ENABLE         = no
 OLED_DRIVER         = SSD1306
 EXTRAKEY_ENABLE     = yes
 LTO_ENABLE          = yes
 QMK_SETTINGS        = yes
-COMBO_ENABLE        = no
-
+COMBO_ENABLE        = yes
