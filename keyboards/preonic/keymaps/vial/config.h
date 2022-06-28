2b55804104d5223ee434259f966ab2a0218d1b46
Alexander <48211557+jedifindtrick@users.noreply.github.com>
2022-06-28 12:37:17 +0200

    Preonic vial commit

---
+++ keyboards/preonic/keymaps/vial/config.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
+
+#ifdef AUDIO_ENABLE
+    #define STARTUP_SONG SONG(PREONIC_SOUND)
+    // #define STARTUP_SONG SONG(NO_SOUND)
+
+    #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND) }
+#endif
+
+#define MUSIC_MASK (keycode != KC_NO)
+
+/*
+ * MIDI options
+ */
+
+/* enable basic MIDI features:
+   - MIDI notes can be sent when in Music mode is on
+*/
+
+#define MIDI_BASIC
+#define VIAL_KEYBOARD_UID {0xB1, 0xED, 0xEE, 0x50, 0xB0, 0x9D, 0xE8, 0xB6}
+/* enable advanced MIDI features:
+   - MIDI notes can be added to the keymap
+   - Octave shift and transpose
+   - Virtual sustain, portamento, and modulation wheel
+   - etc.
+*/
+//#define MIDI_ADVANCED
+
+/* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
+//#define MIDI_TONE_KEYCODE_OCTAVES 2
+
---
+++ keyboards/preonic/keymaps/vial/rules.mk
@@ -0,0 +1,7 @@
+SRC += muse.c
+
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIAL_INSECURE = yes
+LTO_ENABLE = yes
+VIAL_ENCODERS_ENABLE = yes
